3f8e23e06ac4d0f420b6ef026242883a12bd7e43
Wilson <zhaowilson22@gmail.com>
2025-02-22 16:01:22 -0500

    lol cavan

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Cavan has this many braincells:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
