3cfaebf2160ce1727461c5d6ce14f88c2743e952
Wilson <zhaowilson22@gmail.com>
2025-02-22 22:23:26 -0500

    added basic google maps api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePageContent extends StatelessWidget {
+class HomePageContent extends StatefulWidget {
+  @override
+  _HomePageContentState createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  late GoogleMapController _controller;
+
+  // Define the initial camera position
+  final CameraPosition _initialPosition = const CameraPosition(
+    target: LatLng(37.4221, -122.0841),
+    zoom: 15,
+  );
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home - Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: _initialPosition,
+      ),
+    );
+  }
+}
+
+/*class HomePageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,4 +45,4 @@ class HomePageContent extends StatelessWidget {
       ),
     );
   }
-}
+}*/
