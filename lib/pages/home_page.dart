02a0fd3a643995f7750a243d695516b6270b35fc
Wilson <zhaowilson22@gmail.com>
2025-02-23 02:42:22 -0500

    added users live location to map

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:location/location.dart';
 
 class HomePageContent extends StatefulWidget {
@@ -11,6 +11,7 @@ class HomePageContent extends StatefulWidget {
 
 class _HomePageContentState extends State<HomePageContent> {
   late GoogleMapController _controller;
+  final Location _location = Location();
 
   // Define the initial camera position
   final CameraPosition _initialPosition = const CameraPosition(
@@ -18,17 +19,66 @@ class _HomePageContentState extends State<HomePageContent> {
     zoom: 15,
   );
 
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
+
+  // Request location permissions if needed
+  Future<void> _checkLocationPermission() async {
+    bool serviceEnabled = await _location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await _location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    PermissionStatus permissionGranted = await _location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await _location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+  }  
+
   void _onMapCreated(GoogleMapController controller) {
     _controller = controller;
+
+    // Get the current location once the map is created and move the camera
+    _location.getLocation().then((locationData) {
+      if (locationData.latitude != null && locationData.longitude != null) {
+        _controller.animateCamera(
+          CameraUpdate.newLatLng(
+            LatLng(locationData.latitude!, locationData.longitude!),
+          ),
+        );
+      }
+    });
+
+    // Listen to location changes and update the camera
+    _location.onLocationChanged.listen((locationData) {
+      if (locationData.latitude != null && locationData.longitude != null) {
+        _controller.animateCamera(
+          CameraUpdate.newLatLng(
+            LatLng(locationData.latitude!, locationData.longitude!),
+          ),
+        );
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home - Map'),
+        title: const Text('Home'),
       ),
       body: GoogleMap(
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
         onMapCreated: _onMapCreated,
         initialCameraPosition: _initialPosition,
       ),
